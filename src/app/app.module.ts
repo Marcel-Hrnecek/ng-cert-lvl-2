77b138d28776beca608c6b4e5464d5d49986a595
Marcel Hrnecek <marcel.hrnecek@tado.com>
2022-01-17 17:28:38 +0100

    add routing and extract WeatherOverviewComponent
    STEP #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
-import { HttpClientModule } from '@angular/common/http';
+import { WeatherOverviewComponent } from './components/weather-overview/weather-overview.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WeatherOverviewComponent
+  },
+  {
+    path: 'forecast/:zipCode',
+    component: WeatherForecastComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes, {
+      relativeLinkResolution: 'legacy'
+    }),
   ],
   declarations: [
     AppComponent,
     WeatherDetailsComponent,
     WeatherForecastComponent,
+    WeatherOverviewComponent,
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/weather-overview/weather-overview.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { finalize, Subscription } from 'rxjs';
+import { ZipCodeService } from '../../services/zip-code.service';
+import { WeatherService } from '../../services/weather.api.service';
+
+@Component({
+  selector: 'app-weather-overview',
+  templateUrl: './weather-overview.component.html',
+  styleUrls: ['./weather-overview.component.less']
+})
+export class WeatherOverviewComponent implements OnInit, OnDestroy {
+
+  readonly subscriptions: { [key: string]: Subscription } = {};
+
+  isLoading: boolean;
+  weatherList: WeatherData[];
+
+  constructor(
+    private zipCodeService: ZipCodeService,
+    private weatherService: WeatherService
+  ) {
+  }
+
+  ngOnInit(): void {
+    const zipCodes = this.zipCodeService.getZipCodes();
+    if (zipCodes.length) {
+      this.isLoading = true;
+      this.subscriptions.weatherList = this.weatherService.getWeatherListByZipCodes(zipCodes)
+        .pipe(
+          finalize(() => this.isLoading = false)
+        )
+        .subscribe(weatherList => this.weatherList = weatherList);
+    }
+  }
+
+  addLocation(value: string) {
+    if (value && value.length > 0 && !this.zipCodeService.isZipCodeAlreadyStored(value)) {
+      // TODO: Visualize loading state and error handling
+      this.isLoading = true;
+      this.subscriptions.loadWeather = this.weatherService.getWeatherByZipCode(value)
+        .pipe(
+          finalize(() => this.isLoading = false)
+        )
+        .subscribe((weatherData) => {
+          this.zipCodeService.addZipCode(value)
+          this.weatherList.push(weatherData);
+        });
+    }
+  }
+
+  removeLocation(zipCode: string) {
+    this.weatherList = this.weatherList.filter(weatherData => weatherData.zipCode !== zipCode);
+    this.zipCodeService.removeZipCode(zipCode);
+  }
+
+  weatherTrackByFn(index: number, weatherData: WeatherData): string {
+    return weatherData.zipCode;
+  }
+
+  ngOnDestroy(): void {
+    Object.keys(this.subscriptions).forEach(key => {
+      if (this.subscriptions[key]) {
+        this.subscriptions[key].unsubscribe();
+        delete this.subscriptions[key];
+      }
+    });
+  }
+}
