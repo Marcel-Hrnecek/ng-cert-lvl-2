3b2f48d692e106db050b1dfc44fc5780c76fc56b
Marcel Hrnecek <marcel.hrnecek@tado.com>
2022-01-17 14:54:40 +0100

    add HttpClientModule
    STEP #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
