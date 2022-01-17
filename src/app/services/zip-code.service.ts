9b3f2fd3d439a522cb4a3f8706efb7d03cb69f8e
Marcel Hrnecek <marcel.hrnecek@tado.com>
2022-01-17 14:57:38 +0100

    extend ZipCodeService
    STEP #2

--- src/app/services/zip-code.service.ts
+++ src/app/services/zip-code.service.ts
@@ -11,16 +11,27 @@ export class ZipCodeService {
   }
 
   addZipCode(zipCode: string): void {
-    const currentZipCodes = this.getZipCodes();
-    if (currentZipCodes.includes(zipCode)) {
+    if (this.isZipCodeAlreadyStored(zipCode)) {
       return;
     }
+    const currentZipCodes = this.getZipCodes();
     currentZipCodes.push(zipCode);
+    localStorage.setItem(ZipCodeService.ZIP_CODE_STORAGE_KEY, JSON.stringify(currentZipCodes));
+  }
+
+  removeZipCode(zipCode: string): void {
+    let currentZipCodes = this.getZipCodes();
+    currentZipCodes = currentZipCodes.filter(zc => zc !== zipCode);
     localStorage.setItem(ZipCodeService.ZIP_CODE_STORAGE_KEY, JSON.stringify(currentZipCodes));
   }
 
   getZipCodes(): string[] {
     const currentZipCodesStored = localStorage.getItem(ZipCodeService.ZIP_CODE_STORAGE_KEY);
     return currentZipCodesStored ? JSON.parse(currentZipCodesStored) : [];
+  }
+
+  isZipCodeAlreadyStored(zipCode: string): boolean {
+    const currentZipCodes = this.getZipCodes();
+    return currentZipCodes.includes(zipCode);
   }
 }
