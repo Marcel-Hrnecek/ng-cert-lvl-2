914a687775966990f7565d11198298f51292672f
Marcel Hrnecek <marcel.hrnecek@tado.com>
2022-01-18 08:25:07 +0100

    integrate functionality to show weather forecast
    STEP #3

--- src/app/components/weather-forecast/weather-forecast.component.ts
+++ src/app/components/weather-forecast/weather-forecast.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, Subscription, switchMap, tap } from 'rxjs';
+import { WeatherService } from '../../services/weather.api.service';
 
 @Component({
   selector: 'app-weather-forecast',
   templateUrl: './weather-forecast.component.html',
   styleUrls: ['./weather-forecast.component.less']
 })
-export class WeatherForecastComponent implements OnInit {
+export class WeatherForecastComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  readonly subscriptions: { [key: string]: Subscription } = {};
 
-  ngOnInit(): void {
+  isLoading = true;
+
+  zipCode: string;
+  forecastData: ForecastData;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private weatherService: WeatherService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.subscriptions.routeParams = this.route.params
+      .pipe(
+        map(params => params.zipCode),
+        filter(zipCode => !!zipCode),
+        tap(zipCode => this.zipCode = zipCode),
+        switchMap(zipCode => this.weatherService.getForecastByZipCode(zipCode))
+      )
+      .subscribe((forecastData: ForecastData) => {
+        this.forecastData = forecastData;
+        this.isLoading = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    Object.keys(this.subscriptions).forEach(key => {
+      if (this.subscriptions[key]) {
+        this.subscriptions[key].unsubscribe();
+        delete this.subscriptions[key];
+      }
+    });
+  }
+
+  backToMain() {
+    this.router.navigate(['']);
+  }
 }
